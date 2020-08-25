35db62a4dddbec518ea6229583397de4cfb8be9c
nitze <aana.schneider@gmail.com>
2020-08-25 12:55:49 +0900

    dump

--- DevOps/recursion.py
+++ DevOps/recursion.py
@@ -1,40 +1,84 @@
 import os
+import argparse
 
 
-def hierarchy_initializer(depth):
-    path = os.getcwd()
-    if depth >= 1:
-        for levels in range(depth):
-            if levels == 0:
-                level_0_path = os.path.join(path, str(levels))
-                if not os.path.exists(level_0_path):
-                    print(f'makedir, case 0: ', level_0_path)
-                    os.makedirs(str(level_0_path))
-                    f = open(f'{level_0_path}/path.txt', 'a')
-                    f.write(os.path.relpath(str(level_0_path)))
-                    f.close()
-            else:
-                subfolders = []
-                for level in range(levels):
-                    subfolders.extend([x[0] for x in os.walk(str(level))])
-                for subfolder in subfolders[level-1:]:
-                    for l in range(level+1):
+class HierarchyInitializerRecursive:
+    def __init__(self, depth):
+        self.depth = depth
+        self.hierarchy_initializer(self.depth)
+
+    @staticmethod
+    def create_folder(path):
+        """
+        create folder from path if not exist
+        :param path: str
+        :return:
+        """
+        if not os.path.exists(path):
+            os.makedirs(path)
+            # create folders and files
+            f = open(f'{path}/path.txt', 'a')
+            f.write(os.path.relpath(path))
+            f.close()
+
+    def folder_counter(self):
+        """
+        count the number of folder to create
+        :return:
+        """
+        counter = 1
+        result = []
+        for num in range(self.depth+1):
+            res = (num+1) * counter
+
+            result.append(res)
+            counter = res
+
+        return sum(result)
+
+    def hierarchy_initializer(self, depth):
+        """
+        Create a folder/file hierarchy
+        :param depth: int
+        :return:
+        """
+        # get the root path
+        path = os.getcwd()
+
+        # recursive  // remove the 0 value
+        if depほじth >= 1:
+            # for each step/deep
+            for level in range(depth+1):
+                # create the folder 0
+                if level == 0:
+                    level_0_path = os.path.join(path, str(level))  # define path for folder 0 only
+                    self.create_folder(str(level_0_path))  # create folder if not exist
+                else:
+                    # find the past level subfolder
+                    for subfolder in [x[0] for x in os.walk(str(0))]:
+                        # take only the subfolder of this level
                         if len(subfolder.split('\\')) == level:
-                            tt_path = os.path.join(path, subfolder)  # concatenate root path and subfolder
-                            tmp_path = os.path.join(tt_path, str(l))  # concatenate tmp path new folder
-                            # create folder if not exist
-                            if not os.path.exists(tmp_path):
-                                print(f'makedir: ', tmp_path)
-                                os.makedirs(tmp_path)
-                                # create folders and files
-                                f = open(f'{tmp_path}/path.txt', 'a')
-                                f.write(os.path.relpath(str(tmp_path)))
-                                f.close()
-        return depth * hierarchy_initializer(depth - 1)
-    else:
-        return 1
+                            # for each past level
+                            for current_level in range(level+1):
+                                print(current_level, level, depth)
+                                tmp_path = os.path.join(path, subfolder)  # concatenate root path and subfolder
+                                level_path = os.path.join(tmp_path, str(current_level))  # then with new folder
+                                print(level_path)
+                                self.create_folder(str(level_path))  # create folder if not exist
+
+            # return the number of folder to create on the next level
+            return depth * self.hierarchy_initializer(depth - 1)
+        else:
+            return 1
 
 
-for depth in range(6):
-    n = hierarchy_initializer(depth+1)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Hierarchy initializer.')
+    parser.add_argument('--depth', '-d', type=int, help='Depth number', required=True)
 
+    args = parser.parse_args()
+
+    hi = HierarchyInitializerRecursive(args.depth)
+
+    # count the number of folder who need to be created
+    print(f'{hi.folder_counter()} folder created')
