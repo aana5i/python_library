2c8321c81873acc44fcc0115f3d7c012767bf5f4
Goethe <rama.ahn@gmail.com>
2020-09-30 00:38:48 +0900

    proto3

--- importer/importer_saver.py
+++ importer/importer_saver.py
@@ -3,9 +3,17 @@ import pickle
 
 # LOADER
 def from_text(path, mode='r', encoding='utf-8'):
+    """
+    get all line from a text file
+    :param path: str
+    :param mode: str
+    :param encoding: str
+    :return: list
+    """
     _encoding = ['utf-8', 'shift_jis']
     with open(path, mode=mode, encoding=[_encoding[encoding] if isinstance(encoding, int) else encoding][0]) as f:
-        result = f.read()  # str
+        result = f.read()
+        f.close()
     return result
 
 
@@ -28,7 +36,15 @@ def to_pickle(path, filename, mode='wb'):
 
 
 def to_text(path, data, mode='a', encoding='utf-8'):
+    """
+    write one line in a text file
+    :param path: str
+    :param data: str
+    :param mode: str
+    :param encoding: str
+    """
     _encoding = ['utf-8', 'shift_jis']
     with open(path, mode, encoding=[_encoding[encoding] if isinstance(encoding, int) else encoding][0]) as f:
-        f.write(data)
+        f.write(f'{data}\r')
+        f.close()
 
