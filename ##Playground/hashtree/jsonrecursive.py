10e80cf477f2677fecb1cb7d233afc2b20600df1
nitze <aana.schneider@gmail.com>
2020-11-13 16:54:04 +0900

    recursive

--- ##Playground/hashtree/jsonrecursive.py
+++ ##Playground/hashtree/jsonrecursive.py
@@ -3,7 +3,7 @@ import requests
 
 
 
-def compare_hash_trees(key, local_json, server_json):
+def compare_hash_trees(key, local_json, server_json, tmp=None):
     """
     recursively compare local and server json
     :param key: str  'path'
@@ -11,17 +11,19 @@ def compare_hash_trees(key, local_json, server_json):
     :return: list
     """
     results = []
+    if tmp:
+        server_json[key] = tmp
+    if 'size' in local_json:
+        if key not in server_json:
+            results.append(local_json['path'].encode('utf-8'))
 
-    if key not in server_json:
-        results.append(local_json['path'].encode('utf-8'))
-
-    elif local_json[key] != server_json[key]:
-        results.append(local_json['path'].encode('utf-8'))
+        elif local_json[key] not in server_json[key]:
+            results.append(local_json['path'].encode('utf-8'))
 
     if 'children' in local_json and local_json['children']:
-        # for d in local_json['children']:
+        tmp = [hash['hash'] for hash in server_json['children']]
         for folder, folder2 in zip(local_json['children'], server_json['children']):
-            for result in compare_hash_trees(key, folder, folder2):
+            for result in compare_hash_trees(key, folder, folder2, tmp):
                 results.append(result)
 
     return results
@@ -40,17 +42,6 @@ def compare_hash_trees(key, local_json, server_json):
 #             for [key, value], [key2, value2] in zip(folder.items(), folder2.items()):
 #                 print(key, value, key2, value2)
 #                 break
-#
-#
-#
-#
-#
-#
-#             # for result in compare_hash_trees(key, child, server_json['children']):
-#             #     print(result)
-#             # if len(local_json['children']) != len(server_json['children']):
-#             #     print(len(local_json['children']) != len(server_json['children']))
-#
 #
 #     return result
 
@@ -86,10 +77,13 @@ def test_platform(platform):
     connection.close()
 
     f2 = open('iphonemerkleTreeHash.json', "r")
+
     # compareParsedJson(ota_server_curr_root_hash, json.loads(f2.read()))
     # print(prepare_path_from_json2('hash', json.loads(f.read())))
     # print(compare_hash_trees('hash', connection.json(), json.loads(f2.read())))
+
     print(compare_hash_trees('hash', ota_server_curr_root_hash, json.loads(f2.read())))
+
     # print(list(differences(ota_server_curr_root_hash, json.loads(f2.read()))))
 
     f2.close()
